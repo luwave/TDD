b05f6db42d904d4b4286567d22653acd81ee2c0e
luwave <1173403064@qq.com>
2017-01-22 15:53:20 +0800

    test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -5,13 +5,13 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-
+        self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title)
+        self.assertIn('Django',self.browser.title)
         self.fail('Finish the test')
 
 
